78ef5ea6ddbcc2b363f7d09ed8411a87e822b4bf
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2024-12-31 23:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let inputtype = document.querySelector('input');
+inputtype.addEventListener("click",remove);
+
+function remove()
+{
+	let selectedcolor = document.querySelector('#colorSelect');
+	let selectedoption = selectedcolor.options[selectedcolor.selectedIndex];
+
+	if(selectedoption){
+		selectedcolor.removeChild(selectedoption);
+	}
+}
